dfd87c02c6c7a5bed9c3616706fc8f9d3122531a
juanmzabala <juanmzabala@gmail.com>
2023-04-27 12:31:03 -0300

    Update

--- src/components/TagNames.jsx
+++ src/components/TagNames.jsx
@@ -1,16 +1,19 @@
-import '../stylesheets/TagNames.css';
-function TagNames(props){
-    return(
-        <div className={props.className === undefined ? 'tag-name-container' :`tag-name-container ${props.className}`}>
-            <div className="name-display">
-                {props.name}
-            </div>
-            <button className={props.circle} onClick={()=>props.borrar(props.id)}>
-                X
-            </button>
-        </div>
-    )
-};
-
+import "../stylesheets/TagNames.css";
+function TagNames(props) {
+  return (
+    <div
+      className={
+        props.className === undefined
+          ? "tag-name-container"
+          : `tag-name-container ${props.className}`
+      }
+    >
+      <div className="name-display">{props.name}</div>
+      <button className={props.circle} onClick={() => props.borrar(props.id)}>
+        X
+      </button>
+    </div>
+  );
+}
 
 export default TagNames;
